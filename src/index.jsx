3f330256c2fdb13b1b6eacc901d1add3936d455a
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-14 11:42:03 +0200

    route path added to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
-import Survey from './pages/Survey';
-import {
-    createBrowserRouter,
-    RouterProvider
-  } from "react-router-dom";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Header from './components/Header'
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/survey",
-      element: <Survey />
-    }
-  ]);
-  
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
-
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+       <Header />
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/survey" element={<Survey />} />      
+        </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
